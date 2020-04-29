f5d29e14eb72233c2f0123247c01fe98db3062b0
cmhovel <cmhovel@student.ysu.edu>
2020-04-29 17:37:51 -0400

    added the basics if directories aka step6

---
+++ Operating-Systems/include/Directories.h
@@ -0,0 +1,35 @@
+//
+// Created by csis on 4/29/20.
+//
+
+#ifndef INC_5806OSPROJECT_DIRECTORIES_H
+#define INC_5806OSPROJECT_DIRECTORIES_H
+
+#include <cstdint>
+#include "Inode.h"
+
+struct Dirent{
+    uint32_t iNum;
+    uint16_t recLen;
+    uint8_t nameLen;
+    uint8_t fileType;
+    uint8_t name[1];
+};
+struct Directory{
+    uint32_t cursor;
+    uint8_t *blockNum;
+    uint32_t iNum;
+    InodeStruct iS;
+    uint8_t *pointer;
+    Dirent* dirent;
+    ///might need an ext2file inside here
+};
+class Directories {
+    struct Directory *openDirectory(Ext2File* ext2, uint32_t iNum);
+    bool getNextDirent(struct Directory *d, uint32_t &iNum, char*name);
+    void rewindDir(struct Directory *d);
+    void closeDir(struct Directory *d);
+};
+
+
+#endif //INC_5806OSPROJECT_DIRECTORIES_H
---
+++ Operating-Systems/src/Directories.cpp
@@ -0,0 +1,5 @@
+//
+// Created by csis on 4/29/20.
+//
+
+#include "../include/Directories.h"
