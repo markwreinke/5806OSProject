dd7428b5fcdf5cc003eb546ac60f035bfab0c5fd
cmhovel <cmhovel@student.ysu.edu>
2020-04-30 15:22:57 -0400

    traverse path

--- Operating-Systems/src/FilePaths.cpp
+++ Operating-Systems/src/FilePaths.cpp
@@ -2,4 +2,22 @@
 // Created by csis on 4/30/20.
 //
 
+#include <cstring>
 #include "../include/FilePaths.h"
+uint32_t FilePaths::searchDirectory(Ext2File *ext2, uint32_t iNum, char* targetFile){
+
+}
+uint32_t FilePaths::traversePath(Ext2File *ext2, char* path){
+    int start = 1;
+    uint32_t length = strlen(path);
+    uint32_t iNum = 2;
+    while(start < length && iNum != 0){
+        uint32_t end = start + 1;
+        while(path[end] != 0 && path[end]  != '/')
+            end++;
+        path[end] = 0;
+        iNum = searchDirectory(ext2,iNum,path + start);
+        start = end + 1;
+    }
+    return iNum;
+}
