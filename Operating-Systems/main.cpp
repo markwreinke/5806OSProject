7757e8052b84853e54e52dd3ea3a30f47044ec16
Mark Reinke <mark.w.reinke.51@gmail.com>
2020-05-04 00:13:29 -0400

    update main 2

--- Operating-Systems/main.cpp
+++ Operating-Systems/main.cpp
@@ -15,6 +15,16 @@ using namespace std;
 
 
 int main(int argc, char* argv[]) {
+    if(argc <= 1) {
+        cout << "Unbelievably unacceptable arguments. To copy a file from the VDI filesystem to the Host system, you must type: \"-tohost\" followed by the paths for: VDI file, then the source file from the VDI system, then the destination in the host system. The destination path must include the name that you want the file to have (even if you want it to be named the same as its counterpart within the VDI file system)" << endl;
+        cout << endl;
+        cout << "To copy a file from the Host system to the VDI file system, you must type: \"-tovdi\" followed by the paths for VDI file and then the source file from the host system." << endl;
+        cout << endl;
+        cout << "To display the VDI file system, you must type: \"-display\" followed by the path for the VDI file." << endl;
+        cout << endl;
+        cout << "Have a good day, and shoot for the stars." << endl;
+    }
+
    if(strcmp(argv[1], "-tohost")){
        if(argc != 5) {
            cout << "Inadequate arguments. To copy a file from the VDI filesystem to the Host system, you must type: \"-tohost\" followed by the paths for: VDI file, then the source file from the VDI system, then the destination in the host system. The destination path must include the name that you want the file to have (even if you want it to be named the same as its counterpart within the VDI file system)" << endl;
